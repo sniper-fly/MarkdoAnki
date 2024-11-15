f479e64a61e8cc478fb774558bddec46a8ce2656
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-15 15:53:21 +0900

    move comment

--- lib/extractAnkiId.ts
+++ lib/extractAnkiId.ts
@@ -1,3 +1,17 @@
+/*
+mdファイルの形式
+---
+tags:
+  - mysql
+AnkiID:
+  1234
+---
+
+ファイルの先頭に---があり、その中にAnkiIDが記述されている
+AnkiIDが存在しない場合もある
+---が存在しない場合もある
+*/
+
 export function extractAnkiId(data: string): string | null {
   // --- を見つけたら、その中にAnkiIDがあるかを確認する
   const frontMatter = data.match(/---\n([\s\S]*?)---/);
--- lib/retrieveCurrentAnkiIds.ts
+++ lib/retrieveCurrentAnkiIds.ts
@@ -1,20 +1,6 @@
 import { readdir, readFileSync } from "fs";
 import { extractAnkiId } from "./extractAnkiId";
 
-/*
-mdファイルの形式
----
-tags:
-  - mysql
-AnkiID:
-  1234
----
-
-ファイルの先頭に---があり、その中にAnkiIDが記述されている
-AnkiIDが存在しない場合もある
----が存在しない場合もある
-*/
-
 export function retrieveCurrentAnkiIds(path: string): Set<string> {
   let ankiIds: Set<string> = new Set();
   // path に存在する.mdファイルを全て読み込む
