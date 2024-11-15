59b00dc43869e359831cfde684c7124e5e66ba71
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-15 14:16:58 +0900

    complete implementing delete AnkiCards

--- lib/deleteAnkiCards.ts
+++ lib/deleteAnkiCards.ts
@@ -1,1 +1,13 @@
-export function deleteAnkiCards() {}
+import { invokeAnkiApi } from "./invokeAnkiApi";
+
+export async function deleteAnkiCards(ids: string[]) {
+  // string Ids を number に変換する
+  const numIds = ids.map((id) => Number(id));
+  // Number に変換できないものがある場合はエラーを返す
+  if (numIds.some((id) => isNaN(id))) {
+    throw new Error("Invalid Ids");
+  }
+
+  const res = await invokeAnkiApi("deleteNotes", { notes: numIds });
+  return res;
+}
