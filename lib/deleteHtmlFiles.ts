7cc1abacc9ffbb7387ea12ebb2d21f0bac63eefe
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-15 14:00:55 +0900

    id一覧からhtmlを削除する関数を実装

--- lib/deleteHtmlFiles.ts
+++ lib/deleteHtmlFiles.ts
@@ -1,1 +1,14 @@
-export function deleteHtmlFiles() {}
+import { unlink } from "fs";
+
+export function deleteHtmlFiles(ids: string[], path: string) {
+  // ファイルを削除する
+  ids.forEach((id) => {
+    unlink(`${path}/${id}.html`, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`Deleted ${id}.html`);
+    });
+  });
+}
