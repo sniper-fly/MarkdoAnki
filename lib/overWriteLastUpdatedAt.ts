6d788c7317d146f138e9b7320b8b3ad7a0fc6aec
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-16 17:24:42 +0900

    前回のupdate時刻を上書きする関数の作成

--- lib/overWriteLastUpdatedAt.ts
+++ lib/overWriteLastUpdatedAt.ts
@@ -1,2 +1,10 @@
+import { writeFileSync } from "fs";
+
 export function overWriteLastUpdatedAt() {
+  // lastUpdatedAt を更新する
+  const lastUpdatedAt = new Date();
+  writeFileSync(
+    "lastUpdatedAt.ts",
+    `export const lastUpdatedAt = new Date("${lastUpdatedAt.toISOString()}");`
+  );
 }
