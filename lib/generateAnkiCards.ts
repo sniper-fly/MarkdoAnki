c27ceebfc3b9c4aed0ff9cb5e2a7daf4cf28b4dc
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-19 12:53:56 +0900

    AnkiIDをノートに直接付与する仕様をやめる

--- lib/generateAnkiCards.ts
+++ lib/generateAnkiCards.ts
@@ -1,10 +1,11 @@
 import { extractAnkiId } from "./extractAnkiId";
-import { readFileSync, writeFileSync } from "fs";
+import { mkdir, mkdirSync, readFileSync, writeFileSync } from "fs";
 import { extractTags } from "./extractTags";
 import { mdToHtml } from "./mdToHtml";
 import { insertAnkiID } from "./insertAnkiID";
 import { Config } from "../config";
 import { addAnkiNote, updateAnkiNote } from "./ankiNoteApiOperation";
+import { join } from "path";
 
 type Props = {
   notes: string[];
@@ -14,6 +15,7 @@ type Props = {
 export async function generateAnkiCards({
   notes,
   previousFilename2AnkiId,
+  ankiIdRecordPath,
   vaultPath,
   notesPath,
   deck,
@@ -21,25 +23,33 @@ export async function generateAnkiCards({
 }: Props) {
   // NoteにAnkiIDが付与されていれば、AnkiIDをファイル名としてHTMLファイルを出力
   // そうでなければ、HTMLファイルを出力、Ankiカードを作成してからAnkiIDを.mdファイルに付与して保存
-
+  const newFilename2AnkiId = {} as Record<string, number>;
   for (const note of notes) {
     const notePath = `${notesPath}/${note}`;
     const data = readFileSync(notePath, "utf8");
-    // const ankiId = extractAnkiId(data);
-    const ankiId = previousFilename2AnkiId[note];
     const tags = extractTags(data);
     const html = await mdToHtml(data, `${notesPath}/${note}`, vaultPath);
+    let ankiId = previousFilename2AnkiId[note];
     if (ankiId) {
       if (ankiId === -1) {
+        newFilename2AnkiId[note] = ankiId;
         continue;
       }
       await updateAnkiNote(ankiId, note, html, tags);
-      // writeFileSync(`${htmlGenPath}/${ankiId}.html`, html);
     } else {
-      const ankiId = await addAnkiNote(deck, modelName, note, html, tags);
-      // writeFileSync(`${htmlGenPath}/${ankiId}.html`, html);
-      // AnkiIDを.mdファイルに付与して保存
-      // writeFileSync(notePath, insertAnkiID(data, ankiId));
+      ankiId = await addAnkiNote(deck, modelName, note, html, tags);
     }
+    newFilename2AnkiId[note] = ankiId;
   }
+
+  // __previousCardIdsRecord__.md にファイル名とAnkiIDを保存
+  const CardIdsRecordStr = Object.entries(newFilename2AnkiId)
+    .map(([filename, id]) => `${id}/[[${filename}]]`)
+    .join("\n");
+  // ディレクトリが存在しない場合は作成
+  mkdirSync(ankiIdRecordPath, { recursive: true });
+  writeFileSync(
+    join(ankiIdRecordPath, "__previousCardIdsRecord__.md"),
+    CardIdsRecordStr
+  );
 }
