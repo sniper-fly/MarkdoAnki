c0d4a9bb57a6f6890e4158999507fb1d54a6186c
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-19 16:20:10 +0900

    filename と記載している箇所をtitleに変更

--- lib/generateAnkiCards.ts
+++ lib/generateAnkiCards.ts
@@ -8,12 +8,12 @@ import { addAnkiNote, updateAnkiNote } from "./ankiNoteApiOperation";
 import { join } from "path";
 
 type Props = {
-  notes: string[];
+  noteTitles: string[];
   previousNoteTitle2AnkiId: Record<string, number>;
 } & Omit<Config, "createAllCards">;
 
 export async function generateAnkiCards({
-  notes,
+  noteTitles,
   previousNoteTitle2AnkiId,
   ankiIdRecordPath,
   vaultPath,
@@ -23,27 +23,27 @@ export async function generateAnkiCards({
 }: Props) {
   // NoteにAnkiIDが付与されていれば、AnkiIDをファイル名としてHTMLファイルを出力
   // そうでなければ、HTMLファイルを出力、Ankiカードを作成してからAnkiIDを.mdファイルに付与して保存
-  const newFilename2AnkiId = {} as Record<string, number>;
-  for (const note of notes) {
-    const notePath = `${notesPath}/${note}`;
+  const newTitle2AnkiId = {} as Record<string, number>;
+  for (const title of noteTitles) {
+    const notePath = `${notesPath}/${title}`;
     const data = readFileSync(notePath, "utf8");
     const tags = extractTags(data);
-    const html = await mdToHtml(data, `${notesPath}/${note}`, vaultPath);
-    let ankiId = previousFilename2AnkiId[note];
+    const html = await mdToHtml(data, `${notesPath}/${title}`, vaultPath);
+    let ankiId = previousNoteTitle2AnkiId[title];
     if (ankiId) {
       if (ankiId === -1) {
-        newFilename2AnkiId[note] = ankiId;
+        newTitle2AnkiId[title] = ankiId;
         continue;
       }
-      await updateAnkiNote(ankiId, note, html, tags);
+      await updateAnkiNote(ankiId, title, html, tags);
     } else {
-      ankiId = await addAnkiNote(deck, modelName, note, html, tags);
+      ankiId = await addAnkiNote(deck, modelName, title, html, tags);
     }
-    newFilename2AnkiId[note] = ankiId;
+    newTitle2AnkiId[title] = ankiId;
   }
 
   // __previousCardIdsRecord__.md にファイル名とAnkiIDを保存
-  const CardIdsRecordStr = Object.entries(newFilename2AnkiId)
+  const CardIdsRecordStr = Object.entries(newTitle2AnkiId)
     .map(([filename, id]) => `${id}/[[${filename}]]`)
     .join("\n");
   // ディレクトリが存在しない場合は作成
