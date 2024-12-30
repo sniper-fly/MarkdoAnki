df23d5c2fa336e58d306b73599aad0beef0cfa1d
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-12-30 12:47:39 +0900

    import方法変更

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { MarkdoAnki } from "./lib/MarkdoAnki";
 import { config } from "./config";
-import notifier from "node-notifier";
+import { notify } from "node-notifier";
 
 (async () => {
   try {
     await MarkdoAnki(config);
-    notifier.notify({
+    notify({
       title: "MarkdoAnki",
       message: "Anki cards have been created",
       sound: true, // Only Notification Center or Windows Toasters
@@ -14,7 +14,7 @@ import notifier from "node-notifier";
   } catch (e) {
     const error = e as Error;
     console.error(error);
-    notifier.notify({
+    notify({
       title: "MarkdoAnki Error",
       message: error.message || "An error occurred",
       sound: true, // Only Notification Center or Windows Toasters
